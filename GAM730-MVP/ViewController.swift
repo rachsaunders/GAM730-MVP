c6e179a521f3679abacafaca67721dc6a9564ba4
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-07-27 14:41:17 +0100

    Initial Commit

---
+++ GAM730-MVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GAM730-MVP
+//
+//  Created by Rachel Saunders on 27/07/2020.
+//  Copyright © 2020 Rachel Saunders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
