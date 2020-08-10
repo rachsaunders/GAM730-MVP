9c2c627948c67bd0470a07cb72f00eec90b2de41
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-08-10 14:14:38 +0100

    Dismisses Keyboard, Checks text fields all have inputs, added gender options

--- GAM730-MVP/LoginController.swift
+++ GAM730-MVP/LoginController.swift
@@ -5,9 +5,8 @@
 //  Created by Rachel Saunders on 03/08/2020.
 //  Copyright © 2020 Rachel Saunders. All rights reserved.
 //
-
-import Foundation
 import UIKit
+import ProgressHUD
 
 class LoginController: UIViewController {
     
@@ -18,36 +17,63 @@ class LoginController: UIViewController {
     
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    
     //MARK: - ViewLifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
        overrideUserInterfaceStyle = .dark
+        setupBackgroundTouch()
         
     }
     
     //MARK: - IBActions
     
     @IBAction func forgotPasswordButtonPressed(_ sender: Any) {
+        
+        if emailTextField.text != "" {
+            // reset password
+        } else {
+            
+            ProgressHUD.showError("Please insert your email address")
+        }
     }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
+        
+        if emailTextField.text != "" && passwordTextField.text != "" {
+                  // login
+              } else {
+                 
+                  ProgressHUD.showError("All fields are required")
+              }
+    }
+    
+    //MARK: - Setup
+    
+    // Need to set this background touch so that the keyboard can be coded to disappear when user stops typing
+    private func setupBackgroundTouch() {
+        // user can interact with background image now:
+        backgroundImageView.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(backgroundTap))
+        backgroundImageView.addGestureRecognizer(tapGesture)
+        
+    }
+    
+    @objc func backgroundTap() {
+        dismissKeyboard()
     }
     
     
+    //MARK: - Helpers
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    private func dismissKeyboard() {
+        self.view.endEditing(false)
+        
+    }
     
     
     
--- GAM730-MVP/SignUpController.swift
+++ GAM730-MVP/SignUpController.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2020 Rachel Saunders. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import ProgressHUD
 
 class SignUpController: UIViewController {
     
@@ -28,6 +28,12 @@ class SignUpController: UIViewController {
     
     @IBOutlet weak var genderSegmentOutlet: UISegmentedControl!
     
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    //MARK: - Variables
+    
+    // Man, Woman, NonBinary
+    var isMan = true
     
     //MARK: - ViewLifeCycle
        
@@ -35,11 +41,70 @@ class SignUpController: UIViewController {
            super.viewDidLoad()
         
         overrideUserInterfaceStyle = .dark
-    
+
+        setupBackgroundTouch()
         
        }
     
     //MARK: - IBActions
+    
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+        
+        if isTextDataInputed(){
+            registerUser()
+        } else {
+            ProgressHUD.showError("Please fill all info")
+        }
+        
+    }
+    
+    
+    @IBAction func genderSegmentValue(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            isMan = true
+        } else {
+            isMan = false
+        }
+        
+        isMan = sender.selectedSegmentIndex == 0 ? true : false
+        
+    }
+    
+    //MARK: - Setup
+      
+      // Need to set this background touch so that the keyboard can be coded to disappear when user stops typing
+      private func setupBackgroundTouch() {
+          // user can interact with background image now:
+          backgroundImageView.isUserInteractionEnabled = true
+          let tapGesture = UITapGestureRecognizer(target: self, action: #selector(backgroundTap))
+          backgroundImageView.addGestureRecognizer(tapGesture)
+          
+      }
+      
+      @objc func backgroundTap() {
+          dismissKeyboard()
+      }
+      
+      
+      //MARK: - Helpers
+      
+      private func dismissKeyboard() {
+          self.view.endEditing(false)
+          
+      }
+    
+    // check the user has inputted all their data
+    private func isTextDataInputed() -> Bool {
+        return fullNameTextField.text != "" && emailTextField.text != "" && countryTextField.text != "" && passwordTextField.text != "" && confirmPasswordTextField.text != "" && dateOfBirthTextField.text != ""
+    }
+      
+    
+    // MARK: - Register The User
+    
+    private func registerUser() {
+        
+    }
     
 
     
